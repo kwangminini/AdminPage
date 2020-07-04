25be164d7bddc50cde2cab01a48a673a5282a75a
kwangminini <rhkdals7362@gmail.com>
2020-07-04 22:04:32 +0900

    GET & POST Method

---
+++ src/main/java/com/example/admin/controller/GetController.java
@@ -0,0 +1,30 @@
+package com.example.admin.controller;
+
+import com.example.admin.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class GetController {
+
+    @RequestMapping(path = "/getMethod", method = RequestMethod.GET)
+    public String getRequqest(){
+        return "Hi getMethod";
+    }
+
+    @GetMapping("/getParameter")
+    public String getParameter(@RequestParam String id, @RequestParam(name = "password") String pwd){
+        String password="bbbb";
+        System.out.println("id: "+id);
+        System.out.println("password: "+pwd);
+        return id+pwd;
+    }
+    @GetMapping("/getMultiParameter")
+    public SearchParam getMultiParameter(SearchParam searchParam){
+        System.out.println(searchParam.getAccount() );
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+
+        return searchParam;
+    }
+}
---
+++ src/main/java/com/example/admin/controller/PostController.java
@@ -0,0 +1,16 @@
+package com.example.admin.controller;
+
+import com.example.admin.model.SearchParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+    @PostMapping("/postMethod")
+    public SearchParam postMethod(@RequestBody SearchParam searchParam){
+        return searchParam;
+    }
+}
