676bdd04ed34a4ce371e456ff1425a82c6781248
kwangminini <rhkdals7362@gmail.com>
2020-07-11 22:31:52 +0900

    create User API

--- src/main/java/com/example/admin/controller/api/UserApiController.java
+++ src/main/java/com/example/admin/controller/api/UserApiController.java
@@ -3,29 +3,39 @@ package com.example.admin.controller.api;
 
 import com.example.admin.ifs.CrudInterface;
 import com.example.admin.model.network.Header;
+import com.example.admin.model.network.request.UserApiRequest;
+import com.example.admin.model.network.response.UserApiResponse;
+import com.example.admin.service.UserApiLogicService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
-public class UserApiController implements CrudInterface {
+public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
 
+    @Autowired
+    private UserApiLogicService userApiLogicService;
 
     @Override
     @PostMapping("") // /api/user
-    public Header create() {
-        return null;
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+        log.info("{}",request);
+        return userApiLogicService.create(request);
     }
 
     @Override
     @GetMapping("{id}") // /api/user/{id}
-    public Header read(@PathVariable(name="id") Long id) {
-        return null;
+    public Header<UserApiResponse> read(@PathVariable(name="id") Long id) {
+        log.info("read : {}",id);
+        return userApiLogicService.read(id);
     }
 
     @Override
     @PutMapping("") // /api/user
-    public Header update() {
-        return null;
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
+        return userApiLogicService.update(request);
     }
 
     @Override
---
+++ src/main/java/com/example/admin/service/UserApiLogicService.java
@@ -0,0 +1,100 @@
+package com.example.admin.service;
+
+import com.example.admin.ifs.CrudInterface;
+import com.example.admin.model.entity.User;
+import com.example.admin.model.network.Header;
+import com.example.admin.model.network.request.UserApiRequest;
+import com.example.admin.model.network.response.UserApiResponse;
+import com.example.admin.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service //프로젝트의 서비스 로직 담당 , 데이터 변경 수정 생성
+public class UserApiLogicService implements CrudInterface<UserApiRequest, UserApiResponse> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //1. request data
+    //2. user 생성
+    //3. 생성된 데이터 -> UserApiResponse return
+    @Override
+    public Header<UserApiResponse> create(Header<UserApiRequest> request) {
+        //1. request data
+        UserApiRequest userApiRequest = request.getData();
+
+        //2. user생성
+        User user = User.builder()
+                        .account(userApiRequest.getAccount())
+                        .password(userApiRequest.getPassword())
+                        .status("REGISTERED")
+                        .phoneNumber(userApiRequest.getPhoneNumber())
+                        .email(userApiRequest.getEmail())
+                        .registeredAt(LocalDateTime.now())
+                        .build();
+
+        User newUser = userRepository.save(user);
+
+        return response(newUser);
+    }
+
+    @Override
+    public Header<UserApiResponse> read(Long id) {
+
+        // id -> repository getOne, getById
+        Optional<User> optional = userRepository.findById(id);
+        // user -> userApiResponse return
+
+        return optional
+                .map(user -> response(user))
+                .orElseGet(()->Header.ERROR("데이터 없음"));
+
+    }
+
+    @Override
+    public Header<UserApiResponse> update(Header<UserApiRequest> request) {
+        // 1. data
+        UserApiRequest userApiRequest = request.getData();
+
+        // 2. id -> user 데이터를 찾고
+        Optional<User> optional = userRepository.findById(userApiRequest.getId());
+        return optional.map(user -> {
+            // 3.update
+            user.setAccount(user.getAccount())
+                    .setPassword(user.getPassword())
+                    .setStatus(user.getStatus())
+                    .setPhoneNumber(user.getPhoneNumber())
+                    .setEmail(user.getEmail())
+                    .setRegisteredAt(user.getRegisteredAt())
+                    .setUnregisteredAt(user.getUnregisteredAt());
+            
+        })
+                .orElseGet(()->Header.ERROR("데이터 없음"));
+
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+    private Header<UserApiResponse> response(User user){
+        //user -> userApiResponse
+
+        UserApiResponse userApiResponse = UserApiResponse.builder()
+                .id(user.getId())
+                .account(user.getAccount())
+                .password(user.getPassword()) //todo 암호화, 길이제한
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .status(user.getStatus())
+                .registeredAt(user.getRegisteredAt())
+                .unregisteredAt(user.getUnregisteredAt())
+                .build();
+
+        return Header.OK(userApiResponse);
+    }
+}
